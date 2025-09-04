b6f52068434ac945c4dbcd98f250115d0e8d9814
Vinícius Cardoso da Rosa <vinidarosa.for@gmail.com>
2025-09-04 14:05:22 -0300

    Update Soma.cpp

--- Soma.cpp
+++ Soma.cpp
@@ -2,16 +2,16 @@
 
 int main()
 {
-    int n1, n2, conta;
-   printf("Digite um número inteiro: ");
-   scanf("%i",&n1);
-   printf("Digite outro número inteiro: ");
-   scanf("%i",&n2);
-   conta = n1 + n2;
-   if (conta>=10){
-       printf("conta = %i", conta);
-   }else{
-       printf("o número é menor que 10");
+    int n1, n2, conta; //define como int
+   printf("Digite um número inteiro: ");    //print mensagem
+   scanf("%i",&n1);    //pede o n1
+   printf("Digite outro número inteiro: "); //print mensagem
+   scanf("%i",&n2);    //pede o n2
+   conta = n1 + n2;    //conta
+   if (conta>=10){    //se conta maior ou igual a 10
+       printf("conta = %i", conta); //mostra isso
+   }else{    //se não
+       printf("o número é menor que 10");    //mostra isso
    }
 
     return 0;
